cd48bed6d88aa01e2607f4afc6753a58c6b9ef22
Rafael Rahn <rafael.rahn@live.com>
2020-01-19 12:24:32 +0100

    error loging

--- src/app/home/personnel/personnel.service.ts
+++ src/app/home/personnel/personnel.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {PersonnelEntity} from './personnel.entity';
 import {Injectable} from '@angular/core';
+import {catchError, tap} from 'rxjs/operators';
 
 const API_URL = 'http://localhost:8080/personnel';
 
@@ -11,7 +12,13 @@ export class PersonnelService {
   }
 
   loadPersonnel(): Promise<PersonnelEntity[]> {
-    return this.http.get<PersonnelEntity[]>(API_URL).toPromise();
+
+    return this.http.get<PersonnelEntity[]>(API_URL)
+      .pipe(
+        tap( () => {}, error => console.log('e:', error))
+      )
+      .toPromise();
+
   }
 
   addPersonnel(p: PersonnelEntity): Promise<PersonnelEntity> {
